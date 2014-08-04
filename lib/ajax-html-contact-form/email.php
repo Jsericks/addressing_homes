f496d0ea4545cad64dc7b171d21887ed51d3d9b5
Scott Erickson <jsericks@gmail.com>
2014-08-04 16:50:45 -0600

    changed foot city - email properties

--- lib/ajax-html-contact-form/email.php
+++ lib/ajax-html-contact-form/email.php
@@ -3,11 +3,15 @@ $subject = $_REQUEST['subject'] . ' : Addressing Homes Contact Form'; // Subject
 $to = "pcannon@addressinghomes.org";  //Recipient's E-mail
 
 $headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= "From: " . $_REQUEST['emailaddress'] . "\r\n"; // Sender's E-mail
+$headers .= "From: " . $_REQUEST['email'] . "\r\n"; // Sender's E-mail
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-$message .= 'Name: ' . $_REQUEST['name'] . "<br>";
-$message .= 'Message: ' . $_REQUEST['message'];
+$message .= 'Name: ' . $_REQUEST['name'] . "<br><br>";
+$message .= 'Message: ' . $_REQUEST['message'] . "<br><br>";
+$message .= 'Phone: ' . $_REQUEST['phone'] . "<br><br>";
+$message .= 'Email: ' . $_REQUEST['email'] . "<br><br>";
+$message .= 'Country: ' . $_REQUEST['country'] . "<br><br>";
+
 
 if (@mail($to, $subject, $message, $headers))
 {
